15a0088a97f59d3fd40fbba9fcd1f82c009faf0d
arunjadhav009 <githubwork2026@gmail.com>
2026-09-03 16:20:51 +0530

    Update Composition duration in Root component

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,33 +1,11 @@
-import React from "react";
-import { Composition } from "remotion";
-import { OnionRateVideo } from "./Composition";
-
-// डेटा थेट src/data.json फाईलमधून वाचणे
-let dynamicData;
-try {
-  dynamicData = require("./data.json");
-} catch (e) {
-  dynamicData = {
-    date: "02/09/2026",
-    state: "महाराष्ट्र",
-    records: []
-  };
-}
-
-export const RemotionRoot: React.FC = () => {
-  return (
-    <>
-      <Composition
-        id="OnionRateComposition"
-        component={OnionRateVideo}
-        durationInFrames={690}
-        fps={30}
-        width={1080}
-        height={1920}
-        defaultProps={{
-          data: dynamicData
-        }}
-      />
-    </>
-  );
-};
+<Composition
+  id="OnionRateComposition"
+  component={OnionRateVideo}
+  durationInFrames={840} // २८ सेकंद (30 fps * 28s)
+  fps={30}
+  width={1080}
+  height={1920}
+  defaultProps={{
+    data: dynamicData
+  }}
+/>
